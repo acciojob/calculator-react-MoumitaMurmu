838710606be505ccd6f78f28e4201a59f8a34a12
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-06-14 15:16:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,45 @@
-// <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
+import '../styles/App.css';
+import Button from './Button';
 
-    import React, { useState } from 'react';
+const App = () => {
 
-function App() {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
-
-  const handleClick = (value) => {
-    setInput((prevInput) => prevInput + value);
-  };
-
-  const handleClear = () => {
-    setInput('');
-    setResult('');
-  };
-
-  const handleEquals = () => {
-    try {
-      const evalResult = eval(input);
-      setResult(evalResult);
-    } catch (error) {
-      setResult('Error');
+    const data = ["C", "/", "*", "-", "7", "8", "9", "+", "4", "5", "6", "1", "2", "3", "=", "0", ".", ""]
+    let [exp, setExp] = useState("");
+    
+    function handleClick(id) {
+        if (id === "=") {
+            try {
+                console.log(eval(exp));
+                if (eval(exp)!="undefined") {
+                    setExp(eval(exp))
+                }
+            }catch (error) {
+                setExp("Error");
+              }
+           
+           
+        } else if (id=="C") {
+            setExp("");
+        } else {
+            setExp(() => exp + id);
+        }    
     }
-  };
 
-  return (
-    <div className="calculator">
-      <h1>Calculator</h1>
-      <input type="text" value={input} readOnly />
-      <div className="buttons">
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={() => handleClick('+')}>+</button>
-        <button onClick={() => handleClick('-')}>-</button>
-        <button onClick={() => handleClick('*')}>*</button>
-        <button onClick={() => handleClick('/')}>/</button>
-        <button onClick={handleClear}>Clear</button>
-        <button onClick={handleEquals}>=</button>
-      </div>
-      <div className="result">Result: {result}</div>
-    </div>
-  );
+    console.log(exp);
+    return (
+        <div className='container Calculator'>
+            <div className='expression' id="calci">{exp}</div>
+            <div className="button-container">
+                {data.map((key) => (
+                    <Button key={key} id={key}  handleClick={handleClick}></Button>
+                ))}
+            </div>
+        </div>
+
+
+    )
 }
 
+
 export default App;
-
-
